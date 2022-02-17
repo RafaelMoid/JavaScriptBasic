4fe63193714cc71a24a0bb39b2ffd7aff2432e73
Rafael Varela <rafaelvrcr@gmail.com>
2022-02-17 10:20:28 -0300

    Creating arrays with arrays

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,11 @@
+const alunos = ['joão', 'caio', 'joana'];
+
+const mediaDosAlunos = [10, 7 ,9];
+
+let listaDeNotasEAlunos = [alunos , mediaDosAlunos];
+
+console.log(alunos);
+console.log(mediaDosAlunos);
+console.log(listaDeNotasEAlunos);
+
+console.log(`A media de ${listaDeNotasEAlunos[0][0]} é ${listaDeNotasEAlunos[1][0]}`)
