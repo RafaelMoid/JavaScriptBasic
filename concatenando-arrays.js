d63063f65b17e55bbd6c21dec33aa0dcb273a22e
Rafael Varela <rafaelvrcr@gmail.com>
2022-02-17 09:50:33 -0300

    Concatening 2 arrays into new one

---
+++ concatenando-arrays.js
@@ -0,0 +1,7 @@
+const salaDePython = ['ricardo', 'rafael', 'deborah'];
+const salaDeJS = ['pablo', 'laura', 'sandrinha'];
+
+const salasUnificadas = salaDePython.concat(salaDeJS);
+console.log(salaDePython)
+console.log(salaDeJS)
+console.log(salasUnificadas)
