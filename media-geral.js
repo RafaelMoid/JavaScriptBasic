bb38e5f0b373ab86642c61686abfb43e6106478a
Rafael Varela <rafaelvrcr@gmail.com>
2022-02-24 15:13:37 -0300

    Reduce observations

--- media-geral.js
+++ media-geral.js
@@ -2,6 +2,8 @@ const salaJS = [7,8,8,7,10,6.5,4,10,7];
 const salaJava = [6,5,8,9,5,6];
 const salaPython = [7,3.5,8,9.5];
 
+//Reduce faz um loop no array e necessita de dois parametros
+//seu retorno é um unico numero
 function mediaSala(notasDaSala){
     const somaDasNotas = notasDaSala.reduce((acum, atual) => 
     atual+acum,0)
