52235cc131371aceb0333644ea25ce5dc4874a00
Rafael Varela <rafaelvrcr@gmail.com>
2022-02-25 10:35:39 -0300

    Navegando em objetos

---
+++ Curso03JS/print-info.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "08883313342",
+    email:"andre@email.com"
+}
+
+console.log(`Bem vindo ${cliente.nome}, seu CPF é o ${cliente.cpf}.`)
