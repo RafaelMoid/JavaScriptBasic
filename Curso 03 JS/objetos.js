a202e404f1caed00a16e80aaf2d61c593d29202a
Rafael Varela <rafaelvrcr@gmail.com>
2022-02-25 10:10:45 -0300

    Criando objetos

---
+++ Curso 03 JS/objetos.js
@@ -0,0 +1,11 @@
+const listaCPFs = ["123456789", "08983118855","08883313342"];
+
+// const cliente = ["nome", "andre", "idade", 36];
+
+const cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "08883313342",
+    email:"andre@email.com"
+}
+
